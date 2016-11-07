738e1876ef7e875594d9bfbf2bdd9d1f5529542c
santosh s c <san.461@gmail.com>
2016-11-07 16:38:31 +0530

    subbmitting with correction

---
+++ Asignment3.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class Asignment3 {
+	
+	int x; int y;
+	String name;
+	public static void main(String args[]) {
+	Asignment3 pnt = new Asignment3();
+	System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	}
+	////Answer is 'D' coz Instance variable of type int and String are initialized to 0 and null respectively
+}
